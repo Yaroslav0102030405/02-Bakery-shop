1d32868b606fdd12a7d6fea5dc8e035f22d13b5d
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-03-07 20:47:11 +0200

    0

--- README.old.md
+++ README.old.md
@@ -4,3 +4,8 @@
 2 Для перевірки типів переданих props команда в терміналі - npm install --save-dev prop-types
 тут є всі їх значення https://www.npmjs.com/package/prop-types
 Prop-types - це опис типів
+
+Props - статический и динамический
+JSX
+XML
+дефолтные пропсы
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PaintingList from "./components/PaintingList";
+// import Panel from "./components/Panel";
+import paintings from "./datajson/paintings.json";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Panel title="Последние новости">
+        <p>fdgdfgfhfghjhjkhjkjhkhjkh</p>
+        <a href="">Читать...</a>
+      </Panel>
+
+      <Panel>
+        <p>fdgdfgfhfghjhjkhjkjhkhjkh</p>
+      </Panel>
+
+      <h1>Главный компонент-контейнер приложения</h1> */}
+      <PaintingList paintings={paintings} />
     </div>
   );
-}
+};
 
 export default App;
