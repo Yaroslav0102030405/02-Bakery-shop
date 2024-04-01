b5e088b84190cd8f842132afb7f335058e164f30
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-04-01 20:47:31 +0300

    tradition styles-components

--- src/pages/Advantages/Advantages.js
+++ src/pages/Advantages/Advantages.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import './advantages.scss';
+// import './advantages.scss';
 import ribbonjson from './../../datajson/Ribbon.json';
 import Ribbon from './../../components/Ribbon/Ribbon';
+
+import styled from 'styled-components';
+
+const SectionAdventures = styled.section`
+  padding: 16px 0;
+
+  @media screen and (min-width: 1280px) {
+    background-color: #819b57;
+  }
+`;
 
 const Advantages = () => {
   return (
     <>
-      <section className="advantages">
+      <SectionAdventures>
         <div className="container">
           <h2 className="visually-hidden">Наші переваги</h2>
           <Ribbon props={ribbonjson} />
         </div>
-      </section>
+      </SectionAdventures>
     </>
   );
 };
--- src/pages/Traditions/Traditions.js
+++ src/pages/Traditions/Traditions.js
@@ -1,50 +1,92 @@
 import React from 'react';
-import './traditions.scss';
+// import './traditions.scss';
+
 import ImageMan from './../../images/baker-man.jpg';
 // import ImageMan1 from './../../../images/baker-man@1x.jpg';
+import styled from 'styled-components';
+
+const SectionTraditions = styled.section`
+  padding-top: 24px;
+
+  @media screen and (min-width: 1280px) {
+    padding-top: 96px;
+  }
+`;
+
+const TraditionsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 40px;
+  align-items: flex-start;
+`;
+
+const TContent = styled.div`
+  max-width: 710px;
+`;
+
+const Ttitle = styled.h2`
+  margin-bottom: 24px;
+  font-size: 32px;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 64px;
+    margin-bottom: 42px;
+  }
+`;
+
+const Timage = styled.img`
+  display: none;
+  border-radius: 400px 400px 0px 0px;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+`;
+
+const TtextUperCase = styled.span`
+  text-transform: uppercase;
+  color: #819b57;
+  font-weight: 600;
+`;
+
+const Ttext = styled.p`
+  max-width: 640px;
+`;
+
+const Ttext2 = styled.p`
+  max-width: 336px;
+`;
+
+const Tbrand = styled.span`
+  color: #819b57;
+  font-weight: 600;
+`;
 
 const Traditions = () => {
   return (
     <>
-      <section className="traditions section" id="traditions">
+      <SectionTraditions className="traditions section" id="traditions">
         <div className="container">
-          <div className="traditions__container">
-            <div className="traditions__content">
-              <h2 className="traditions__title section__title">
+          <TraditionsContainer>
+            <TContent>
+              <Ttitle className="traditions__title section__title">
                 Новий формат традицій
-              </h2>
-              <p className="tranditions__text">
-                <span className="traditions__text--brand tranditions__text--uppercase">
-                  shop bakery
-                </span>
-                — це пекарня, яка взяла все найкраще, і зберегла смак
-                традиційної випічки та натуральність інгредієнтів.
-              </p>
-              <p className="tranditions__text">
+              </Ttitle>
+              <Ttext>
+                <TtextUperCase>shop bakery</TtextUperCase>— це пекарня, яка
+                взяла все найкраще, і зберегла смак традиційної випічки та
+                натуральність інгредієнтів.
+              </Ttext>
+              <Ttext2>
                 Ми працюємо як найвідоміші мережі —
-                <span className="traditions__text--brand">
-                  5 хвилин і замовлення готове
-                </span>
-                . А ще у наших пекарень стильний дизайн та висока якість
-                обслуговування!
-              </p>
-            </div>
-            <img
-              className="traditions__baker"
-              //   srcset="
-              //     ./images/baker-man@1x.jpg 1x,
-              //     ./images/baker-man@2x.jpg 2x
-              //   "
-              //   src="./images/baker-man.jpg"
-              //   srcSet=(ImageMan1)
-              srcSet={ImageMan}
-              //   srcSet=<ImageMan />
-
-              alt="Чоловік пекарь дивится на хліб"
-            />
-          </div>
+                <Tbrand>5 хвилин і замовлення готове</Tbrand>. А ще у наших
+                пекарень стильний дизайн та висока якість обслуговування!
+              </Ttext2>
+            </TContent>
+            <Timage srcSet={ImageMan} alt="Чоловік пекарь дивится на хліб" />
+          </TraditionsContainer>
         </div>
-      </section>
+      </SectionTraditions>
     </>
   );
 };
